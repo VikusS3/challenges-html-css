22e7efef13b19990d38ba4c870cf055dc8ed90a8
VikusS3 <felix21soc@gmail.com>
2024-03-18 18:10:39 -0500

    fixed

--- Simple-Homepage/index.js
+++ Simple-Homepage/index.js
@@ -15,6 +15,27 @@ console.log(localStorage.getItem('theme'));
 // Funciones para aplicar el tema claro
 function applyLightMode() {
  
+  body.classList.remove("dark-theme");
+  body.classList.add("light-theme");
+  darkModeBtn.style.backgroundColor = "transparent";
+  lightModeBtn.style.backgroundColor = "#f0f0f0";
+  darkModeBtnMobile.style.backgroundColor = "transparent";
+  lightModeBtnMobile.style.backgroundColor = "#f0f0f0";
+  document.querySelector("#dark-mode-btn img").style.display = "inline-block";
+  document.querySelector("#dark-mode-btn .dark-moon").style.display = "none";
+  document.querySelector('#light-mode-btn .dark-sun').style.display = 'none';
+  document.querySelector('#light-mode-btn img').style.display = 'inline-block';
+
+  document.querySelector('#dark-mode-btn-mobile img').style.display = 'inline-block';
+  document.querySelector('#dark-mode-btn-mobile .dark-moon').style.display = 'none';
+  document.querySelector('#light-mode-btn-mobile img').style.display = 'inline-block';
+  document.querySelector('#light-mode-btn-mobile .dark-sun').style.display = 'none';
+
+  
+}
+
+function applyDarkMode() {
+ 
 
 
   body.classList.add("dark-theme");
@@ -32,24 +53,6 @@ function applyLightMode() {
   document.querySelector('#dark-mode-btn-mobile .dark-moon').style.display = 'inline-block';
   document.querySelector('#light-mode-btn-mobile img').style.display = 'none';
   document.querySelector('#light-mode-btn-mobile .dark-sun').style.display = 'inline-block';
-}
-
-function applyDarkMode() {
-  body.classList.remove("dark-theme");
-  body.classList.add("light-theme");
-  darkModeBtn.style.backgroundColor = "transparent";
-  lightModeBtn.style.backgroundColor = "#f0f0f0";
-  darkModeBtnMobile.style.backgroundColor = "transparent";
-  lightModeBtnMobile.style.backgroundColor = "#f0f0f0";
-  document.querySelector("#dark-mode-btn img").style.display = "inline-block";
-  document.querySelector("#dark-mode-btn .dark-moon").style.display = "none";
-  document.querySelector('#light-mode-btn .dark-sun').style.display = 'none';
-  document.querySelector('#light-mode-btn img').style.display = 'inline-block';
-
-  document.querySelector('#dark-mode-btn-mobile img').style.display = 'inline-block';
-  document.querySelector('#dark-mode-btn-mobile .dark-moon').style.display = 'none';
-  document.querySelector('#light-mode-btn-mobile img').style.display = 'inline-block';
-  document.querySelector('#light-mode-btn-mobile .dark-sun').style.display = 'none';
 }
 
 
